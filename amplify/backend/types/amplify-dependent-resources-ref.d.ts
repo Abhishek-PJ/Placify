f5ab05b21081d8e6b6371a72e7abe674cea8f29b
Abhishek-PJ <abhia7535@gmail.com>
2025-08-10 20:51:21 +0530

    Initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "placify7b8d2e98": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "placifyresources": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
